89991c594f148d0e716dba5850e0c408cbcc68a8
Danya-Mudaifea <66493820+Danya-Mudaifea@users.noreply.github.com>
2020-08-12 09:32:29 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
+sudo apt update -y 
+sudo apt install git curl make vim -y
+sudo apt install jq -y
+curl -L get.docker.com |sh
 sudo usermod -aG docker ubuntu
 echo "done."
